16dd36de2292f9cc97811a850531c838e6ed3096
zahidshowrav <zahid.showravbd@gmail.com>
2024-11-28 00:14:35 +0600

    Updated page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,17 @@
 import Image from 'next/image';
+import { useEffect } from 'react';
 
-export default function Home() {
+const Home = () => {
+	const hello = (text: string) => {
+		// eslint-disable-next-line no-console
+		console.log('Hello ', text);
+		return text;
+	};
+
+	useEffect(() => {
+		hello();
+	}, []);
+
 	return (
 		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
 			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -98,4 +109,6 @@ export default function Home() {
 			</footer>
 		</div>
 	);
-}
+};
+
+export default Home;

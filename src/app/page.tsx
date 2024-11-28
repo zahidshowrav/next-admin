74c68474b5180585c2699d15977ea82342d33d2c
zahidshowrav <zahid.showravbd@gmail.com>
2024-11-29 02:20:20 +0600

    Added alert ui component and updated home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect } from 'react';
 
 import { Button } from '@ui/button';
+import { Card } from '@components/ui/card';
 
 const Home = () => {
 	const hello = (text: string) => {
@@ -19,10 +20,20 @@ const Home = () => {
 
 	return (
 		<div className="p-32">
-			<h1>Home Page</h1>
-			<div className="mt-8">
-				<Button>Primary Button</Button>
-			</div>
+			<h1 className="mb-8">Home Page</h1>
+			<Card className="p-4">
+				<h1 className="font-bold text-2xl">Lorem ipsum dolor sit</h1>
+				<p className="mt-3">
+					Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+					Facere dignissimos incidunt quas deleniti exercitationem
+					optio aliquid modi. Asperiores vero accusantium, a minus
+					quibusdam dolores sunt ducimus temporibus illo laudantium
+					ipsum!
+				</p>
+				<div className="mt-8">
+					<Button>Primary Button</Button>
+				</div>
+			</Card>
 		</div>
 	);
 };

c2bce85af7b7dbf83d1b16d50055007eedc3025e
zahidshowrav <zahid.showravbd@gmail.com>
2024-12-02 13:23:04 +0600

    1. Created ThemeSwitcher Component, 2. Created theme provider, 3. Updated dashboard and root layout for adding theme provider and switcher design

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -11,6 +11,7 @@ import {
 } from '@ui/breadcrumb';
 import { Separator } from '@ui/separator';
 import { SidebarInset, SidebarProvider, SidebarTrigger } from '@ui/sidebar';
+import ThemeSwitcher from '@snippets/theme-switcher';
 
 export const metadata: Metadata = {
 	title: 'Public Layout Title'
@@ -27,27 +28,32 @@ const DashboardLayout = ({
 			<SidebarInset>
 				{/* <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12"> */}
 				<header className="sticky top-0 flex shrink-0 items-center gap-2 border-b bg-background p-4">
-					<div className="flex items-center gap-2 pr-4">
-						<SidebarTrigger className="-ml-1" />
-						<Separator
-							orientation="vertical"
-							className="mr-2 h-4"
-						/>
-						<Breadcrumb>
-							<BreadcrumbList>
-								<BreadcrumbItem className="hidden md:block">
-									<BreadcrumbLink href="#">
-										Building Your Application
-									</BreadcrumbLink>
-								</BreadcrumbItem>
-								<BreadcrumbSeparator className="hidden md:block" />
-								<BreadcrumbItem>
-									<BreadcrumbPage>
-										Data Fetching
-									</BreadcrumbPage>
-								</BreadcrumbItem>
-							</BreadcrumbList>
-						</Breadcrumb>
+					<div className="w-full flex items-center gap-4 justify-between">
+						<div className="flex items-center gap-2">
+							<SidebarTrigger className="-ml-1" />
+							<Separator
+								orientation="vertical"
+								className="mr-2 h-4"
+							/>
+							<Breadcrumb>
+								<BreadcrumbList>
+									<BreadcrumbItem className="hidden md:block">
+										<BreadcrumbLink href="#">
+											Building Your Application
+										</BreadcrumbLink>
+									</BreadcrumbItem>
+									<BreadcrumbSeparator className="hidden md:block" />
+									<BreadcrumbItem>
+										<BreadcrumbPage>
+											Data Fetching
+										</BreadcrumbPage>
+									</BreadcrumbItem>
+								</BreadcrumbList>
+							</Breadcrumb>
+						</div>
+						<div className="flex items-center justify-end gap-2">
+							<ThemeSwitcher />
+						</div>
 					</div>
 				</header>
 				<div className="p-4">{children}</div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
+
 import './globals.css';
+import ThemeProvider from '@contexts/theme';
 
 const geistSans = localFont({
 	src: './fonts/GeistVF.woff',
@@ -24,11 +26,21 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
+		<html
+			lang="en"
+			suppressHydrationWarning
+		>
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				{children}
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="light"
+					enableSystem={false}
+					disableTransitionOnChange
+				>
+					{children}
+				</ThemeProvider>
 			</body>
 		</html>
 	);

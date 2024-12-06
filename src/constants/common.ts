cea59749b53b88d8dbf40a9c57f4ad96c746d248
zahidshowrav <zahid.showravbd@gmail.com>
2024-12-07 02:39:16 +0600

    Updated common constants

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,3 +1,10 @@
 export const IS_MULTI_TENANT =
 	process.env.NEXT_PUBLIC_IS_MULTI_TENANT === 'true';
 export const DEFAULT_THEME = process.env.NEXT_PUBLIC_DEFAULT_THEME || 'dark';
+export const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL;
+export const OG_IMAGE = process.env.NEXT_PUBLIC_OG_IMAGE;
+export const WEBSITE_URL = process.env.NEXT_PUBLIC_WEBSITE_URL || '';
+export const TWITTER_URL = process.env.NEXT_PUBLIC_TWITTER_URL || '';
+export const GITHUB_URL = process.env.NEXT_PUBLIC_GITHUB_URL || '';
+export const GITHUB_USERNAME = process.env.NEXT_PUBLIC_GITHUB_USERNAME || '';
+export const GITHUB_REPO_NAME = process.env.NEXT_PUBLIC_REPO_NAME || '';
